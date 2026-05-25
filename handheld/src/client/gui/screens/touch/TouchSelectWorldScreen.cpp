3d60059b86e3d5286fafc39f0b83ffd2b5e28203
Li <li@silica.codes>
2026-05-25 18:26:50 +1200

    fix ANDROID

--- handheld/src/client/gui/screens/touch/TouchSelectWorldScreen.cpp
+++ handheld/src/client/gui/screens/touch/TouchSelectWorldScreen.cpp
@@ -352,7 +352,7 @@ void SelectWorldScreen::buttonClicked(Button* button)
 {
 	if (button->id == bCreate.id) {
 		if (_state == _STATE_DEFAULT && !_hasStartedLevel) {
-#if defined(__ANDROID__) || defined(__APPLE__)
+#if defined(ANDROID) || defined(__APPLE__)
 			minecraft->platform()->createUserInput(DialogDefinitions::DIALOG_CREATE_NEW_WORLD);
 #endif
 			_state = _STATE_CREATEWORLD;
@@ -409,7 +409,7 @@ void SelectWorldScreen::tick()
 			// honestly our custom choose level scren is probably better than whatever win32 does
 			std::string name = getUniqueLevelName("perf");
 			minecraft->setScreen(new SimpleChooseLevelScreen(name));
-		#elif !defined(__ANDROID__) || !defined(__APPLE__)
+		#elif !defined(ANDROID) || !defined(__APPLE__)
 			minecraft->setScreen(new AdvancedChooseLevelScreen());
 			return;
 		#else
